e6aefe0db0efa0cf1a896243d2abc239013d86ee
Gary Verhaegen <gary.verhaegen@gmail.com>
2018-09-30 17:13:15 +0100

    correct URL for text files

--- get-shakes.sh
+++ get-shakes.sh
@@ -9,7 +9,7 @@ cd $DIR
 
 mkdir -p files
 cd files
-curl -s http://www.gutenberg.org/ebooks/1112.txt.utf-8 > romeo-and-juliet.txt
+curl -s http://www.gutenberg.org/cache/epub/1112/pg1112.txt > romeo-and-juliet.txt
 curl -s http://www.gutenberg.org/files/1524/1524-0.txt > hamlet.txt
 curl -s http://www.gutenberg.org/files/23042/23042-0.txt > tempest.txt
-curl -s http://www.gutenberg.org/ebooks/2264.txt.utf-8 > macbeth.txt
+curl -s http://www.gutenberg.org/cache/epub/2264/pg2264.txt > macbeth.txt
